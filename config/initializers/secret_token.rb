627de250ed714d983c05dedff363dd1ef9ea0686
inbar <inbar@agilesparks.com>
2012-07-02 11:26:55 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Work2game::Application.config.secret_token = '988a6c139dd5a43b8a48b8c85eaa45c927babdc7ea134064fd604e35be62499dd6063105c8623acf56961e7e6dce20dfb52699412a6c79ca0217b0e836904b94'
